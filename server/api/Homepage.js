16aeb6cf5bd16f07c1e545007ca3ccfa2432e3ec
@albob105 <albob105@student.liu.se>
2022-10-13 11:43:41 +0200

    changes before merge

---
+++ server/api/Homepage.js
@@ -0,0 +1,22 @@
+const express = require('express'); 
+const router = express.Router(); 
+const db = require('../database')
+var user; 
+const sql1 = "SELECT DISTINCT * FROM MyCourses WHERE (Owner) = (?)"
+
+// Fetching user's courses from database and sending it to HomePage component
+router.get('/Homepage/:owner', async (req, res) => {
+
+    const owner = req.params.owner; 
+    let database = await db.allAsync(sql1, owner);
+    console.log("inside homepage api")
+    console.log(database)
+    return res.json(database); 
+}); 
+
+// Receiving username from component and saving it into variable user
+router.post('/Homepage', async (req,res) => {
+    user = req.body; 
+}); 
+
+module.exports = router; 
